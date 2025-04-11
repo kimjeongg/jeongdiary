5d7d1077f05b3271a9535a3dfe1052028e90123c
kimjeongg <rlawjddl3916@gmail.com>
2025-04-11 10:54:14 +0900

    title change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,49 @@
 import Button from "../component/Button";
 import Header from "../component/Header";
-import { useState,useContext,useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { DiaryStateContext } from '../App';
-import { getMonthRangeDate } from "../util";
+import { getMonthRangeDate, setPageTitle } from "../util";
 import DiaryList from "../component/DiaryList";
 
 const Home = () => {
     const [pivotDate, setPivotDate] = useState(new Date());
     const data = useContext(DiaryStateContext);
     const [filteredData, setFilteredData] = useState([]);
-        const onIncresesMonth = () => {
-        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth()+1));
+    const onIncresesMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
     }
     const onDecresesMonth = () => {
-        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth()-1));
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
     }
-    const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth()+1}월`;
-    useEffect(()=>{
-        if(data.length >=1){
-            const {beginTimeStamp, endTimeStamp} = getMonthRangeDate(pivotDate);
+    const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
+    useEffect(() => {
+        if (data.length >= 1) {
+            const { beginTimeStamp, endTimeStamp } = getMonthRangeDate(pivotDate);
             setFilteredData(
-                data.filter(it=>beginTimeStamp <= it.date && it.date <= endTimeStamp )
+                data.filter(it => beginTimeStamp <= it.date && it.date <= endTimeStamp)
             )
-        }else{
+        } else {
             setFilteredData([]);
         }
-    },[data,pivotDate])
+    }, [data, pivotDate])
+    
+    useEffect(()=>{
+        setPageTitle('정이님의 감정일기장')
+    },[]);
     return (
         <div>
-            <Header 
-            title={headerTitle} 
-            leftChild={
-                <Button 
-                text={'<'}  onClink={onDecresesMonth}/>
-            }
-            rightChild={
-            <Button 
-                text={'>'} onClink={onIncresesMonth}
-               /> }
+            <Header
+                title={headerTitle}
+                leftChild={
+                    <Button
+                        text={'<'} onClink={onDecresesMonth} />
+                }
+                rightChild={
+                    <Button
+                        text={'>'} onClink={onIncresesMonth}
+                    />}
             />
-            <DiaryList data={filteredData}/>
+            <DiaryList data={filteredData} />
         </div>
     )
 }
